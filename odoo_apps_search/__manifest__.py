98b44e19dadd635761d5f3b74a148a19b2f1edd7
hamzbond <hamzbond@gmail.com>
2025-01-09 13:17:04 +0700

    changes for v14

--- odoo_apps_search/__manifest__.py
+++ odoo_apps_search/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Odoo Apps Search Enhancements',
-    'version': '16.0.1.0.0',
+    'version': '14.0.1.0.0',
     'summary': 'Enhancements for searching and filtering apps in Odoo',
     'description': """
         This module adds the following features to the Odoo Apps menu:
