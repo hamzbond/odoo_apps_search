6a48443470e1281d91f42f30b25236c00700ed85
hamzbond <hamzbond@gmail.com>
2025-01-09 09:59:40 +0700

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+{
+    'name': 'Odoo Apps Search Enhancements',
+    'version': '16.0.1.0.0',
+    'summary': 'Enhancements for searching and filtering apps in Odoo',
+    'description': """
+        This module adds the following features to the Odoo Apps menu:
+        - Search by author and website
+        - Filter by demo data
+        - Group by author, website, category, and license
+    """,
+    'author': 'Hamzbond',
+    'website': 'https://hamzbond.github.io',
+    'category': 'Tools',
+    'license': 'LGPL-3',
+    'depends': ['base'],
+    'data': [
+        'views/app_search_views.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
